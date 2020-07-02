d442787de18b21537bafebc8758e28d3c9998326
Allan Gomes <traduvert@icloud.com>
2020-07-02 19:04:58 -0300

    Updating data in DB

--- src/application/InsertIntoDB.java
+++ src/application/InsertIntoDB.java
@@ -5,47 +5,20 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import db.DB;
 
-public class Program {
+public class InsertIntoDB {
 
 	public static void main(String[] args) {
-
-//		Connection con = null;
-//		Statement st = null;
-//		ResultSet rs = null;
-//
-//		try {
-//
-//			con = DB.getConnection();
-//
-//			// Starting Statement
-//			st = con.createStatement();
-//
-//			rs = st.executeQuery("select * from seller");
-//			
-//			while (rs.next()) {
-//				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-//			}
-//
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		} finally {
-//			DB.closeResultSet(rs);
-//			DB.closeStatement(st);
-//			DB.closeConnection();
-//		}
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
+//		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
 		Connection conn = null;
 		PreparedStatement st = null;
 				
 		try {
 			conn = DB.getConnection();
-			
+			/*
 			st = conn.prepareStatement(
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
@@ -61,18 +34,24 @@ public class Program {
 			st.setDate(3, new java.sql.Date(sdf.parse("15/55/1996").getTime()));
 			st.setDouble(4, 5500);
 			st.setInt(5, 4);
+			*/
+			
+			st = conn.prepareStatement(
+					"insert into department (Name) values ('D5'),('D6')",
+					Statement.RETURN_GENERATED_KEYS);
 			
 			int rowsAffected = st.executeUpdate();
-			
+			int lineCounter = 0;
 			if(rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				while(rs.next()) {
+					lineCounter++;
 					//Column 1
 					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id + " Rows affected: " + rowsAffected);
+					System.out.println("Done! Id = " + id + " Rows affected: " + lineCounter);
 				}
 			}
-		} catch (SQLException | ParseException e) {
+		} catch (SQLException  e) {
 			e.printStackTrace();
 		}
 		finally {
@@ -80,4 +59,5 @@ public class Program {
 			DB.closeConnection();
 		}
 	}
+
 }
